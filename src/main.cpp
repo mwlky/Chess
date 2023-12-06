2ad43e702bb05d7bd233189e77b6bca8f08fb3fd
mwlky <mwlky@getgoogleoff.me>
2023-12-06 20:11:10 +0100

    Project init

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <SDL2/SDL.h>
+
+int main(int argc, const char* argv[])
+{
+    bool quit = false;
+    SDL_Event event;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("SDL2 Starter Project",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_EventState(SDL_MOUSEMOTION, SDL_IGNORE);
+
+    while (!quit)
+    {
+        SDL_Delay(10);
+        SDL_PollEvent(&event);
+
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                quit = true;
+                break;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 242, 242, 242, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
