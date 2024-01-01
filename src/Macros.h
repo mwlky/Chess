69e218bb2fa9321ac4846504f223dbe58c7693e9
mwlky <mwlky@getgoogleoff.me>
2024-01-01 19:01:27 +0100

    Release

--- src/Macros.h
+++ src/Macros.h
@@ -6,21 +6,20 @@
 
 #define SQUARE_SIZE 100
 
-#define PAWN_BLACK "../assets/Chess_Pawn_Black.png"
-#define BLACK_SQUARE "../assets/black.png"
-#define ROOK_BLACK "../assets/Chess_Rock_Black.png"
-#define KING_BLACK "../assets/Chess_King_Black.png"
-#define QUEEN_BLACK "../assets/Chess_Hetman_Black.png"
-#define KNIGHT_BLACK "../assets/Chess_Knight_Black.png"
-#define BISHOP_BLACK "../assets/Chess_Bishop_Black.png"
+#define PAWN_BLACK "assets/Chess_Pawn_Black.png"
+#define BLACK_SQUARE "assets/black.png"
+#define ROOK_BLACK "assets/Chess_Rock_Black.png"
+#define KING_BLACK "assets/Chess_King_Black.png"
+#define QUEEN_BLACK "assets/Chess_Hetman_Black.png"
+#define KNIGHT_BLACK "assets/Chess_Knight_Black.png"
+#define BISHOP_BLACK "assets/Chess_Bishop_Black.png"
 
-#define WHITE_SQUARE "../assets/white.png"
-#define KING_WHITE "../assets/Chess_King_White.png"
-#define PAWN_WHITE "../assets/Chess_Pawn_White.png"
-#define ROOK_WHITE "../assets/Chess_Rock_White.png"
-#define QUEEN_WHITE "../assets/Chess_Queen_White.png"
-#define KNIGHT_WHITE "../assets/Chess_Knight_White.png"
-#define BISHOP_WHITE "../assets/Chess_Bishop_White.png"
-
+#define WHITE_SQUARE "assets/white.png"
+#define KING_WHITE "assets/Chess_King_White.png"
+#define PAWN_WHITE "assets/Chess_Pawn_White.png"
+#define ROOK_WHITE "assets/Chess_Rock_White.png"
+#define QUEEN_WHITE "assets/Chess_Queen_White.png"
+#define KNIGHT_WHITE "assets/Chess_Knight_White.png"
+#define BISHOP_WHITE "assets/Chess_Bishop_White.png"
 
 #endif //CHESS_MACROS_H
